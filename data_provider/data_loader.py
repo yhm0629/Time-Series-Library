42c0382a2e2db71301d3c72459e96f005cf67943
Haoming Yang <1557645006@qq.com>
2026-03-23 19:55:30 +0800

    Time-Series-Library

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -851,8 +851,10 @@ class UEAloader(Dataset):
 
 class PEMSLoader(Dataset):
     def __init__(self, args, root_path, flag='train', size=None,
-                 features='M', data_path='PEMS04.npz',
+                 features='S', data_path='ETTh1.csv',
                  target='OT', scale=True, timeenc=0, freq='h', seasonal_patterns=None):
+        
+        self.args = args
         self.seq_len = size[0]
         self.label_len = size[1]
         self.pred_len = size[2]
@@ -861,48 +863,62 @@ class PEMSLoader(Dataset):
         type_map = {'train': 0, 'val': 1, 'test': 2}
         self.set_type = type_map[flag]
 
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+
         self.root_path = root_path
         self.data_path = data_path
-        self.scale = scale
         self.__read_data__()
 
     def __read_data__(self):
         self.scaler = StandardScaler()
-        data = np.load(os.path.join(self.root_path, self.data_path))['data'][:, :, 0]
-        
-        num_train = int(len(data) * 0.6)
-        num_test = int(len(data) * 0.2)
-        num_vali = len(data) - num_train - num_test
-        border1s = [0, num_train - self.seq_len, len(data) - num_test - self.seq_len]
-        border2s = [num_train, num_train + num_vali, len(data)]
-        
-        border1 = border1s[self.set_type]
-        border2 = border2s[self.set_type]
+        data_file = os.path.join(self.root_path, self.data_path)
+        print('data file:', data_file)
+        data = np.load(data_file, allow_pickle=True)
+        data = data['data'][:, :, 0]
+
+        train_ratio = 0.6
+        valid_ratio = 0.2
+        train_data = data[:int(train_ratio * len(data))]
+        valid_data = data[int(train_ratio * len(data)):int((train_ratio + valid_ratio) * len(data))]
+        test_data = data[int((train_ratio + valid_ratio) * len(data)):]
+        total_data = [train_data, valid_data, test_data]
+        data = total_data[self.set_type]
 
         if self.scale:
-            train_data = data[border1s[0]:border2s[0]]
-            self.scaler.fit(train_data)
+            self.scaler.fit(data)
             data = self.scaler.transform(data)
-        
-        self.data_x = data[border1:border2]
-        self.data_y = data[border1:border2]
-        self.data_stamp = np.zeros((len(self.data_x), 4)) 
+
+        df = pd.DataFrame(data)
+        df = df.fillna(method='ffill', limit=len(df)).fillna(method='bfill', limit=len(df)).values
+
+        self.data_x = df
+        self.data_y = df
 
     def __getitem__(self, index):
-        s_begin = index
+        if self.set_type == 2:
+            s_begin = index * 12
+        else:
+            s_begin = index
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
 
         seq_x = self.data_x[s_begin:s_end]
         seq_y = self.data_y[r_begin:r_end]
-        seq_x_mark = self.data_stamp[s_begin:s_end]
-        seq_y_mark = self.data_stamp[r_begin:r_end]
+        seq_x_mark = torch.zeros((seq_x.shape[0], 1))
+        seq_y_mark = torch.zeros((seq_y.shape[0], 1))
 
         return seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+        if self.set_type == 2:
+            return (len(self.data_x) - self.seq_len - self.pred_len + 1) // 12
+        else:
+            return len(self.data_x) - self.seq_len - self.pred_len + 1
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
